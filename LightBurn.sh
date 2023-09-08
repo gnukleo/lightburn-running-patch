6091969e1e2dbc0302d130c907f0504fc19a86dd
gnukleo_ubuntu <leonelmalon@gmail.com>
2023-09-08 15:55:25 -0300

    create a launcher file, with the patch

---
+++ LightBurn.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+prefs_ini=$HOME/.config/LightBurn/prefs.ini
+
+mv $prefs_ini $prefs_ini.old
+
+echo "change values to windows sizes"
+jq '.WindowSizeX=1834 | .WindowSizeY=968' $prefs_ini.old > $prefs_ini
+
+echo "run LightBurn.."
+$HOME/.local/share/LightBurn/LightBurn
