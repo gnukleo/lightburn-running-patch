1327de39a40de0f77341d4334a18ee12dbed475e
gnukleo_ubuntu <leonelmalon@gmail.com>
2024-04-04 10:53:50 -0300

    bugfix when open witha file as argument

--- LightBurn.sh
+++ LightBurn.sh
@@ -7,5 +7,10 @@ mv $prefs_ini $prefs_ini.old
 echo "change values to windows sizes"
 jq '.WindowSizeX=1834 | .WindowSizeY=968' $prefs_ini.old > $prefs_ini
 
-echo "run LightBurn.."
-$HOME/.local/share/LightBurn/LightBurn "$1"
+if [ $# -eq 0 ]; then
+    echo "run LightBurn without arguments.. "
+    $HOME/.local/share/LightBurn/LightBurn
+else
+    echo "run LightBurn with arguments.. "
+    $HOME/.local/share/LightBurn/LightBurn "$1"
+fi
